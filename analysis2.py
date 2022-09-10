7eb2f0ac973050a3602ed4df314b71c1a513e2d6
yassinmelsir <yassinmelsir@gmail.com>
2022-09-10 12:28:05 -0500

    final touches

--- analysis2.py
+++ analysis2.py
@@ -25,17 +25,8 @@ def exit_dist(valuation):
 
     # with a cap of 2x and three rounds of preferred investors at amounts >= 60m, best outcome for investors will always be to convert to common shares 
     if valuation >= 60000000:
-        c_payout += payout_ammount*(c_shares/total_shares)
-        total_shares -= c_shares
-        payout_ammount -= c_payout
-        b_payout += payout_ammount*(b_shares/total_shares)
-        total_shares -= b_shares
-        payout_ammount -= b_payout
-        a_payout += payout_ammount*(a_shares/total_shares)
-        total_shares -= a_shares
-        payout_ammount -= a_payout
-        com_payout += payout_ammount*(com_shares/total_shares)
-        return print(c_payout, b_payout, a_payout, com_payout, valuation == (c_payout + b_payout + a_payout + com_payout))
+        scenario_Two = [payout_ammount*(c_shares/total_shares), payout_ammount*(b_shares/total_shares), payout_ammount*(a_shares/total_shares), payout_ammount*(com_shares/total_shares)]
+        print('all cs', scenario_Two)
     # assumption: each investors attempts to maximize their profits
     # at valuations less than 60m and greater than or equal to 40m it never makes sense for pref C or pref B to take common shares, and B will always Cap.
     # at valulations of more than or equal to 46.2m C will always cap, C will always LP until 60m where CS start too outpace LPs
@@ -77,51 +68,15 @@ def exit_dist(valuation):
         print(scO_payout_ammount,(c_shares/scO_shares))
         scenario_Two = [cap_Table['c']['invested']+(scO_payout_ammount*(c_shares/scO_shares)), b_cap, a_cap, (scO_payout_ammount)*(com_shares/scO_shares)]
         print('r a cap, b cap, C lps: ', scenario_Two)
-        # a cap, b cs, C lps
-        # scTw_payout_ammount = payout_ammount-cap_Table['c']['invested']-cap_Table['a']['invested']*2
-        # scTw_shares = total_shares-cap_Table['a']['shares']
-        # scenario_Two = [cap_Table['c']['invested']+scTw_payout_ammount*(c_shares/scTw_shares), (scTw_payout_ammount)*(b_shares/scTw_shares), a_cap, (scTw_payout_ammount)*(com_shares/scTw_shares)]
-        # print('a cap, b cs, C lps: ', scenario_Two)
-         # a cs, b cap, C lps
-        # scT_payout_ammount = payout_ammount-cap_Table['c']['invested']-cap_Table['b']['invested']*2
-        # scT_shares = total_shares-cap_Table['b']['shares']
-        # scenario_Two = [cap_Table['c']['invested']+scT_payout_ammount*(c_shares/scT_shares), b_cap, (scT_payout_ammount)*(a_shares/scT_shares), (scT_payout_ammount)*(com_shares/scT_shares)]
-        # print('a cs, b cap, C lps: ', scenario_Two)
-        # a cs, b cs, C lps
-        # scF_payout_ammount = payout_ammount-cap_Table['c']['invested']
-        # scO_shares = total_shares
-        # scenario_Two = [cap_Table['c']['invested']+scF_payout_ammount*(c_shares/scO_shares), (scF_payout_ammount)*(b_shares/scO_shares), (scF_payout_ammount)*(a_shares/scO_shares), (scF_payout_ammount)*(com_shares/scO_shares)]
-        # print('C LP, B and A CS: ', scenario_Two)
-
         return None
+
     elif 38500000 >=  valuation >= 31500000:
-        
-        # 4 scenarios here: A and B take common shares, A and B take LPs, A takes Lps and B takes common shares or A takes common shares and B takes Lps
-        # C takes LP, A and B take common shares
-        # scenario_One = [cap_Table['c']['invested']+(payout_ammount-cap_Table['c']['invested'])*(c_shares/total_shares),payout_ammount*(b_shares/total_shares),payout_ammount*(a_shares/total_shares),payout_ammount*(com_shares/total_shares)]
-        # print('C takes LP, A and B take common shares: ', scenario_One)
-
-        # C, B, and A choose LPs
-        # scO_payout_ammount = payout_ammount-cap_Table['c']['invested']-cap_Table['b']['invested']*2-cap_Table['a']['invested']
-        # scO_shares = total_shares - cap_Table['b']['shares']
-        # scenario_Two = [cap_Table['c']['invested']+scO_payout_ammount*(c_shares/scO_shares), b_cap, cap_Table['a']['invested']+scO_payout_ammount*(a_shares/scO_shares), (scO_payout_ammount)*(com_shares/scO_shares)]
-        # print('C, B, and A choose LPs: ', scenario_Two)
-
-        # C and B take LPs, A takes Common shares
-        # scT_payout_ammount = payout_ammount-cap_Table['c']['invested']-cap_Table['b']['invested']
-        # scT_shares = total_shares
-        # scenario_Two = [cap_Table['c']['invested']+scT_payout_ammount*(c_shares/scT_shares), cap_Table['b']['invested']+scT_payout_ammount*(b_shares/scT_shares), scT_payout_ammount*(a_shares/scT_shares), (scT_payout_ammount)*(com_shares/scT_shares)]
-        # print('C, B, choose LPs, A chooses common shares: ', scenario_Two)
-        # C and B take LPs, B takes Common shares
-        # scF_payout_ammount = payout_ammount-cap_Table['c']['invested']-cap_Table['a']['invested']
-        # scenario_Four = [cap_Table['c']['invested']+scF_payout_ammount*(c_shares/total_shares),scF_payout_ammount*(b_shares/total_shares),cap_Table['a']['invested']+scF_payout_ammount*(a_shares/total_shares),scF_payout_ammount*(com_shares/total_shares)]
-        # print('C and A take LPs, B takes Common shares: ', scenario_Four)
-
         scO_payout_ammount = payout_ammount-cap_Table['c']['invested']-cap_Table['b']['invested']-cap_Table['a']['invested']*2
         scO_shares = total_shares-cap_Table['a']['shares']
         scenario_Two = [cap_Table['c']['invested']+scO_payout_ammount*(c_shares/scO_shares), cap_Table['b']['invested']+scO_payout_ammount*(b_shares/scO_shares), a_cap, (scO_payout_ammount)*(com_shares/scO_shares)]
         print('C, B, and A choose LPs: ', scenario_Two)
         return None
+
     elif 31500000 > valuation :
         # at 31.5m A begins to Cap
         scO_payout_ammount = payout_ammount-cap_Table['c']['invested']-cap_Table['b']['invested']-cap_Table['a']['invested']
@@ -131,116 +86,13 @@ def exit_dist(valuation):
         return None
 
         
-
-        
-
-
-    if c_liq_payout > c_cap:
-        if b_liq_payout > b_cap:
-            if a_liq_payout > a_cap:
-                c_payout += payout_ammount*(c_shares/total_shares)
-                total_shares -= c_shares
-                payout_ammount -= c_payout
-                b_payout += payout_ammount*(b_shares/total_shares)
-                total_shares -= b_shares
-                payout_ammount -= b_payout
-                a_payout += payout_ammount*(a_shares/total_shares)
-                total_shares -= a_shares
-                payout_ammount -= a_payout
-                com_payout += payout_ammount*(com_shares/total_shares)
-            else:
-                print('')
-
-        else:
-            if a_liq_payout > a_cap:
-                print('')
-            else:
-                print('')
-    else:
-        if b_liq_payout > b_cap:
-            if a_liq_payout > a_cap:
-                b_payout += b_cap
-                total_shares -= b_shares
-                payout_ammount -= b_payout
-                if payout_ammount*(c_shares/total_shares) > (cap_Table['c']['invested'] + (valuation-cap_Table['c']['invested']-b_payout)*(c_shares/(total_shares))):
-                    c_payout += payout_ammount*(c_shares/total_shares)
-                    total_shares -= c_shares
-                    payout_ammount -= c_payout
-                else: 
-                    c_payout += (cap_Table['c']['invested'] + (valuation-cap_Table['c']['invested']-b_payout)*(c_shares/(total_shares)))
-                    payout_ammount -= cap_Table['c']['invested']
-
-                if (payout_ammount)*(a_shares/total_shares) > (valuation-cap_Table['c']['invested']-b_payout)*(a_shares/total_shares):
-                    print(payout_ammount)
-                    a_payout += payout_ammount*(a_shares/total_shares)
-                else: 
-                    if (cap_Table['a']['invested'] + (valuation-cap_Table['c']['invested']-b_payout)*(a_shares/total_shares)) < a_cap:
-                        a_payout += (cap_Table['a']['invested'] + (valuation-cap_Table['c']['invested']-b_payout)*(a_shares/total_shares))
-                    else: 
-                        a_payout += payout_ammount*(a_shares/total_shares)
-                
-                com_payout = valuation - a_payout - b_payout - c_payout
-            else:
-               print('')
-            
-
-        else:
-            if a_liq_payout > a_cap:
-                a_payout += a_cap
-                total_shares -= a_shares
-                payout_ammount -= a_payout
-                if payout_ammount*(c_shares/total_shares) > (cap_Table['c']['invested'] + (valuation-financed_valuation-a_payout/2)*(c_shares/(total_shares))):
-                    c_payout += payout_ammount*(c_shares/total_shares)
-                    total_shares -= c_shares
-                    payout_ammount -= c_payout
-                else: 
-                    c_payout += (cap_Table['c']['invested'] + (valuation-financed_valuation-a_payout/2)*(c_shares/(total_shares)))
-        
-                if payout_ammount*(b_shares/total_shares) > (cap_Table['b']['invested'] + (valuation-financed_valuation-a_payout/2)*(b_shares/(total_shares))):
-                    b_payout += payout_ammount*(b_shares/total_shares)
-                    total_shares -= b_shares
-                    payout_ammount -= b_payout
-                else: 
-                    b_payout += (cap_Table['b']['invested'] + (valuation-financed_valuation-a_payout/2)*(b_shares/(total_shares)))
-                
-                
-                com_payout = valuation - a_payout - b_payout - c_payout
-            else:
-                if payout_ammount*(c_shares/total_shares) > (cap_Table['c']['invested'] + (valuation-financed_valuation)*(c_shares/(3000000))):
-                    c_payout += payout_ammount*(c_shares/total_shares)
-                    total_shares -= c_shares
-                    payout_ammount -= c_payout
-                else: 
-                    c_payout += (cap_Table['c']['invested'] + (valuation-financed_valuation)*(c_shares/(3000000)))
-        
-                if payout_ammount*(b_shares/total_shares) > (cap_Table['b']['invested'] + (valuation-financed_valuation)*(b_shares/(3000000))):
-                    b_payout += payout_ammount*(b_shares/total_shares)
-                    total_shares -= b_shares
-                    payout_ammount -= b_payout
-                else: 
-                    b_payout += (cap_Table['b']['invested'] + (valuation-financed_valuation)*(b_shares/(3000000)))
-
-                if payout_ammount*(a_shares/total_shares) > (cap_Table['a']['invested'] + (valuation-financed_valuation)*(a_shares/(3000000))):
-                    a_payout += payout_ammount*(a_shares/total_shares)
-                    total_shares -= a_shares
-                    payout_ammount -= a_payout
-                else: 
-                    print('b')
-                    a_payout += (cap_Table['a']['invested'] + (valuation-financed_valuation)*(a_shares/(3000000)))
-                # a_payout += (cap_Table['a']['invested'] + (valuation-financed_valuation)*(a_shares/total_shares))
-                com_payout = valuation - a_payout - b_payout - c_payout
-                
-                
-    print(c_payout, b_payout, a_payout, com_payout, valuation == (c_payout + b_payout + a_payout + com_payout))
-    # (c_payout + b_payout + a_payout)
-    
-# exit_dist(60000000)
-# exit_dist(25000000)
+exit_dist(60000000)
+exit_dist(25000000)
 # exit_dist(31500000)
-# exit_dist(34000000)
+exit_dist(35000000)
 # exit_dist(40000000)
 # exit_dist(37500000)
-# exit_dist(44000000)
+exit_dist(45000000)
 # exit_dist(46200000)
 # C begins to cap......
 # exit_dist(46800000)
